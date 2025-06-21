c809d621ebb3fabf0ed9a21fa984e14125bc4a27
NIHARA W.A.S <sandarunihara56@gmail.com>
2025-06-21 18:57:40 +0530

    Initialize React application with Tailwind CSS setup
    - Created package.json with dependencies for React, testing libraries, and Tailwind CSS.
    - Added public assets including favicon, logos, and manifest for PWA support.
    - Set up index.html as the main entry point for the application.
    - Implemented basic App component structure in App.js.
    - Configured Tailwind CSS in index.css and added necessary styles.
    - Established entry point in index.js for rendering the React application.
    - Created tailwind.config.js for Tailwind CSS configuration.

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# ConstruxFlow
+3rd year group project – construction inventory and supply chain management  system.
---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="bg-black h-screen w-full">
+      
+    </div>
+  );
+}
+
+export default App;
