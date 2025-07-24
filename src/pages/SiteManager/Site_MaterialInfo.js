8d730c62bac03f1e6d89dc1cb502df95187f85b1
Malin Ashmitha Ekanayake <159604336+MalinEkanayake31@users.noreply.github.com>
2025-07-24 13:21:06 +0530

    site inventory

--- src/pages/SiteManager/Site_MaterialInfo.js
+++ src/pages/SiteManager/Site_MaterialInfo.js
@@ -11,7 +11,7 @@ function Site_MaterialInfo() {
           { name: "Dashboard", href: "/site-manager" },
           { name: "Projects", href: "/site-manager/projects-list" },
           { name: "Materials", href: "/site-manager/material-request-list" },
-          { name: "Inventory", href: "/site-manager/site-material-info" },
+          { name: "Inventory", href: "/site-manager/site-inventory" },
           // { name: "Purchase Orders", href: "/site-manager/projects-list" },
         ]}
         // showButton={true}
